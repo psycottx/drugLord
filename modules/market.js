ae75381874ee775d3340ea5a25dd4570c1c3ea13
himan999 <himan8nine@gmail.com>
2015-06-05 16:35:06 +0530

    added random market

--- modules/market.js
+++ modules/market.js
@@ -129,9 +129,37 @@ marketMod.service('drugCityService',['randomService','$log', function(rS,$log){
     mark.cities = ["Austin, USA", "Beijing, China", "Boston, USA", "Detroit, USA", "London, England", "Los Angeles, USA", "Miami, USA", "Moscow, Russia", "New York, USA", "Paris, France", "San Francisco, USA", "St. Peteresburg, Russia", "Sydney, Australia", "Toronto, Canada", "Vancouver, Canada"];
 
     mark.drugs = [];
+    mark.indexes = [];
+    mark.drugs1 = [];
     
     mark.initMarket = function() {
-        mark.drugs = mark.initDrugs();
+        mark.drugs1 = mark.initDrugs();
+        mark.size = Math.round(rS.randomPrice(17));
+        while(mark.size < 7) {
+            mark.size = Math.round(rS.randomPrice(17));
+        }
+        for(var i=0; i < mark.size; i++) {
+            var j = Math.round(rS.randomPrice(16)), flag=0;
+            for(var k = 0; k < mark.indexes.length; k++) {
+                if(mark.indexes[k] == j) {
+                    j = Math.round(rS.randomPrice(16));
+                    k=-1;
+                }
+            }
+            mark.indexes.push(j);
+        }
+        for(var i=0; i < mark.size; i++) {
+            mark.drugs.push(mark.drugs1[mark.indexes[i]]);
+        }
+        mark.drugs.sort(compare);
+        function compare(a,b) {
+            if (a.name < b.name)
+                return -1;
+            if (a.name > b.name)
+                return 1;
+            return 0;
+        }
+        console.log(mark.indexes);
         return true;
     };
     mark.prevTarget = null;
@@ -143,13 +171,20 @@ marketMod.service('drugCityService',['randomService','$log', function(rS,$log){
         if(mark.prevTarget == null) {
             e.currentTarget.className="list-group-item active";
             mark.prevTarget = e.currentTarget;
-        } else {
+        } else if(mark.prevTarget == e.currentTarget) {
+            if(e.currentTarget.className == "list-group-item active") {
+                 e.currentTarget.className = "list-group-item";
+                mark.drugs[index].selected = false;
+            } else {
+                 e.currentTarget.className = "list-group-item active";
+                mark.drugs[index].selected = true;
+            }
+        }
+        else {
             mark.prevTarget.className="list-group-item";
             e.currentTarget.className="list-group-item active";
             mark.prevTarget = e.currentTarget;
         }
-        
-      //  console.log(mark.drugs);
     };
 
 }]);
